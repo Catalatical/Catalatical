6e75575c9f1823806127d7194156650c7b933cf3
Cat <Catalatical@gmail.com>
2024-11-24 23:18:32 +0100

    Add files via upload

--- scrpt.js
+++ scrpt.js
@@ -43,3 +43,47 @@ function openNav() {
     document.querySelector('.Info').style.display = 'none';
   }
 
+
+
+// deay be gone
+
+function showDay(day) {
+  var dayInfo = "day"+day;
+  var dayElement = document.getElementById(dayInfo);
+
+  document.querySelector('.subcard').classList.toggle("shown");
+  document.querySelector('.Close').classList.toggle("CloseAnim");
+  document.querySelector('.Panel').classList.toggle("PanelAnim");
+  var allDayElements = document.querySelectorAll(".day");
+  allDayElements.forEach(function(element) {
+    element.style.display = 'none';
+  });
+
+  dayElement.style.display = 'block';
+}
+
+function closeSub() {
+  
+  document.querySelector('.subcard').classList.remove("shown");
+  document.querySelector('.Close').classList.remove("CloseAnim");
+  document.querySelector('.Panel').classList.remove("PanelAnim");
+
+  document.querySelector('.subcard').classList.add("hidden");
+  document.querySelector('.Close').classList.add("CloseAnimEnd");
+  document.querySelector('.Panel').classList.add("PanelAnimEnd");
+
+  setTimeout(function() {
+    document.querySelector('.subcard').classList.remove("hidden");
+    document.querySelector('.Close').classList.remove("CloseAnimEnd");
+    document.querySelector('.Panel').classList.remove("PanelAnimEnd");
+  }, 1000); // wait for 1 second
+
+  setTimeout(function() {
+    var allDayElements = document.querySelectorAll(".day");
+  allDayElements.forEach(function(element) {
+    element.style.display = 'none';
+  });
+  }, 1000);
+
+}
+
